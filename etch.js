c45c28df3ae1d13373d099bf7d01aae9c4587624
jklxco <jack.a.coleman@hotmail.com>
2022-04-04 13:04:24 +0700

    Switch CSS display to grid

--- etch.js
+++ etch.js
@@ -3,32 +3,27 @@ function createGrid(gridSize) {
     const container = document.querySelector("#grid-container");
     clearGrid(container);
 
-    for (i = 1; i <= gridSize; i++ ) {
-        let divCol = document.createElement("div");
-        divCol.classList.add("box-col");
-        for (j = 1; j <= gridSize; j++ ) {
-            let divRow = document.createElement("div");
-            divRow.classList.add("box");
-            //divRow.textContent = `${i}-${j}`;
-            divRow.addEventListener("mouseenter", function() {
-                divRow.classList.add("hovered")
-            });
-            divCol.appendChild(divRow);
-        };
-        container.appendChild(divCol);
+    for (i = 1; i <= (gridSize*gridSize); i++ ) {
+        let div = document.createElement("div");
+        div.classList.add("box");
+        div.addEventListener("mouseenter", function() {
+            div.classList.add("hovered")
+        });
+        container.appendChild(div);
     };
+    container.style["grid-template-columns"] = `repeat(${gridSize}, 1fr)`;
 }
 
 function refreshGrid() {
     let usedSquares = document.querySelectorAll(".hovered");
     usedSquares.forEach( e => e.classList.remove("hovered") );
-    gridSize = parseInt(prompt("Enter a size for the new grid: (Max: 100) ", 16));
-    //let gridSize = (gridSize > 100) ? 100 : gridSize; 
+    let gridSize = parseInt(prompt("Enter a size for the new grid: (Max: 100) ", 16));
+    if (gridSize > 100 ) gridSize = 100;
     createGrid(gridSize);
 }
 
 function clearGrid(container) {
-    let clearSquares = document.querySelectorAll(".box-col");
+    let clearSquares = document.querySelectorAll(".box");
     clearSquares.forEach(e => container.removeChild(e));
 }
 
